06b4e0e8ba387e71ada62582bec27d119e12b748
Anna <anna_dorofeewa@mail.ru>
2018-06-11 13:02:25 +0300

    исходные данные для объявлений

---
+++ js/map.js
@@ -0,0 +1,48 @@
+'use strict'
+
+var advertisements = [{
+			autor = {
+				avatar: ['img/avatars/user01.png',
+					'img/avatars/user02.png',
+					'img/avatars/user03.png',
+					'img/avatars/user04.png',
+					'img/avatars/user05.png',
+					'img/avatars/user06.png',
+					'img/avatars/user07.png',
+					'img/avatars/user08.png'
+				]
+			},
+			{
+				offer = {
+					title: ['Большая уютная квартира',
+						'Маленькая неуютная квартира',
+						'Огромный прекрасный дворец',
+						'Маленький ужасный дворец',
+						'Красивый гостевой домик',
+						'Некрасивый негостеприимный домик',
+						'Уютное бунгало далеко от моря',
+						'Неуютное бунгало по колено в воде'
+					]
+				},
+				{
+					address: '100, 200',
+					price: ,
+					type: [palace, flat, house, bungalo],
+					rooms: [1, 5],
+					guests: ,
+					checkin: ['12:00', '13:00', '14:00'],
+					checkout: ['12:00', '13:00', '14:00'],
+					features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+					description: '';
+					photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+						'http://o0.github.io/assets/images/tokyo/hotel2.jpg'
+						'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+					]
+				},
+				{
+					location = {
+						x: [300, 900],
+						y: [130, 360]
+					}
+				}
+			];
